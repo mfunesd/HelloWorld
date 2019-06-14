42cf5a6f593ba9ae632a955515e21bf0553556c7
mfunesd <fdmanuel@yahoo.es>
2019-06-14 20:29:22 +0200

    Commit inicial

---
+++ src/main/java/com/code/fremap/HelloWorld/App.java
@@ -0,0 +1,13 @@
+package com.code.fremap.HelloWorld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
